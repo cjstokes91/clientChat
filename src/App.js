ef522e45c2d8989dafc521940a71126de9fd7d3a
carl stokes <carlstokes@carls-MBP.lan>
2020-05-13 15:06:41 -0700

    chat functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Join from './components/Join/Join';
+
 import Chat from './components/Chat/Chat';
+import Join from './components/Join/Join';
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
     return (
@@ -9,7 +11,7 @@ const App = () => {
             <Route path="/" exact component={Join} />
             <Route path="/chat" component={Chat} />
         </Router>
-    )
-};
+    );
+}
 
-export default App; 
+export default App;
